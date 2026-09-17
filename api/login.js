b306f2f744e895285ab3663c79790149be1c3228
Anir <soko84.sd@gmail.com>
2026-09-17 10:04:01 +0800

    Add Vercel login API and update articles

---
+++ api/login.js
@@ -0,0 +1,24 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method not allowed'
+    })
+  }
+
+  const { username, password } = req.body
+
+  if (
+    username === process.env.ADMIN_USERNAME &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    return res.status(200).json({
+      success: true,
+      message: 'Login амжилттай'
+    })
+  }
+
+  return res.status(401).json({
+    success: false,
+    error: 'Username эсвэл password буруу'
+  })
+}
